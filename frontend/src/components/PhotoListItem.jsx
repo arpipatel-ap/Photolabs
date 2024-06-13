b2d83d18cb8fbb75325b975ba15a50addc2cacaa
arpipatel-ap <arpi96@gmail.com>
2024-06-13 16:27:19 -0400

    add styling classname in jsx

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -2,14 +2,15 @@ import React from "react";
 import "../styles/PhotoListItem.scss";
 
 const PhotoListItem = ({ username, imageSource, id, location, profile }) => {
+ 
   return (
-    <div>
-      <img src={imageSource} alt ="Image" />
-      <img src={profile} alt= "Profile" />
-      <div>
-        <h2>{username}</h2>
-        <p>Location: {location.city}, {location.country}</p>
-      </div>
+    <div className="photo-list__item">
+      <img className= " .photo-list__image"src={imageSource} alt ="Image" />
+      <img className = ".photo-list__user-profile"src={profile} alt= "Profile" />
+      
+        <h2 className="photo-list__user-info ">{username}</h2>
+        <p className=".photo-list__user-location">Location: {location.city}, {location.country}</p>
+      
     </div>
   );
 };
